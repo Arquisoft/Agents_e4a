872345f4d68b35c39c8011b740fbd91097d0c943
PSB <sierrabonet@gmail.com>
2018-02-05 10:35:40 +0100

    Actualizado el modelo

--- src/main/java/asw/dbManagement/model/Participant.java
+++ src/main/java/asw/dbManagement/model/Participant.java
@@ -1,6 +1,5 @@
 package asw.dbManagement.model;
 
-import java.util.Date;
 
 import javax.persistence.Column;
 import javax.persistence.Entity;
@@ -18,143 +17,136 @@ public class Participant {
 	private Long id;
 
 	// Atributos del participante
-	private String nombre;
-	private String apellidos;
-	private String password;
-	private Date fechaNacimiento;
-	@Column(unique = true)
-	private String email;
-	@Column(unique = true)
-	private String DNI;
-	private String direccion;
-	private String nacionalidad;
+		private String nombre;
+		private String localizacion;
+		@Column(unique = true)
+		private String email;
+		@Column(unique = true)
+		private String identificador;
+		private String tipo;
+		private int tipoCode;
+		private String password;
 
-	private boolean isAdmin;
-	private boolean isPolitician;
+		/**
+		 * Constructor vacío (ya que es para mapear)
+		 */
+		Participant() {
+		}
 
-	/**
-	 * Constructor vacío (ya que es para mapear)
-	 */
-	Participant() {
-	}
+		/**
+		 * Constructor
+		 * 
+		 * @param nombre
+		 * @param localizacion
+		 * @param email
+		 * @param identificador
+		 * @param tipo
+		 * @param tipoCode
+		 * @param password
+		 */
 
-	/**
-	 * Constructor
-	 * 
-	 * @param nombre
-	 * @param apellidos
-	 * @param password
-	 * @param fechaNacimiento
-	 * @param email
-	 * @param dNI
-	 * @param direccion
-	 * @param nacionalidad
-	 */
-	public Participant(String nombre, String apellidos, String password, Date fechaNacimiento, String email, String dNI,
-			String direccion, String nacionalidad, boolean isAdmin, boolean isPolitician) {
-		super();
-		this.nombre = nombre;
-		this.apellidos = apellidos;
-		this.password = password;
-		this.fechaNacimiento = fechaNacimiento;
-		this.email = email;
-		this.DNI = dNI;
-		this.direccion = direccion;
-		this.nacionalidad = nacionalidad;
-		this.isAdmin = isAdmin;
-		this.isPolitician = isPolitician;
-	}
+		public Participant(String nombre, String localizacion, String email, String identificador, String tipo,
+				int tipoCode, String password) {
+			super();
+			this.nombre = nombre;
+			this.localizacion = localizacion;
+			this.email = email;
+			this.identificador = identificador;
+			this.tipo = tipo;
+			this.tipoCode = tipoCode;
+			this.password = password;
+		}
 
-	public Long getId() {
-		return id;
-	}
+		public Long getId() {
+			return id;
+		}
 
-	public String getNombre() {
-		return nombre;
-	}
+		public String getNombre() {
+			return nombre;
+		}
 
-	public String getApellidos() {
-		return apellidos;
-	}
+		public void setNombre(String nombre) {
+			this.nombre = nombre;
+		}
 
-	public String getPassword() {
-		return password;
-	}
+		public String getLocalizacion() {
+			return localizacion;
+		}
 
-	public void setPassword(String password) {
-		this.password = password;
-	}
+		public void setLocalizacion(String localizacion) {
+			this.localizacion = localizacion;
+		}
 
-	public Date getFechaNacimiento() {
-		return fechaNacimiento;
-	}
+		public String getEmail() {
+			return email;
+		}
 
-	public String getEmail() {
-		return email;
-	}
+		public void setEmail(String email) {
+			this.email = email;
+		}
 
-	public void setEmail(String email) {
-		this.email = email;
-	}
+		public String getIdentificador() {
+			return identificador;
+		}
 
-	public String getDNI() {
-		return DNI;
-	}
+		public void setIdentificador(String identificador) {
+			this.identificador = identificador;
+		}
 
-	public String getDireccion() {
-		return direccion;
-	}
+		public String getTipo() {
+			return tipo;
+		}
 
-	public String getNacionalidad() {
-		return nacionalidad;
-	}
+		public void setTipo(String tipo) {
+			this.tipo = tipo;
+		}
 
-	public boolean isAdmin() {
-		return isAdmin;
-	}
+		public int getTipoCode() {
+			return tipoCode;
+		}
 
-	public void setAdmin(boolean isAdmin) {
-		this.isAdmin = isAdmin;
-	}
+		public void setTipoCode(int tipoCode) {
+			this.tipoCode = tipoCode;
+		}
 
-	public boolean isPolitician() {
-		return isPolitician;
-	}
+		public String getPassword() {
+			return password;
+		}
 
-	public void setPolitician(boolean isPolitician) {
-		this.isPolitician = isPolitician;
-	}
+		public void setPassword(String password) {
+			this.password = password;
+		}
 
-	@Override
-	public int hashCode() {
-		final int prime = 31;
-		int result = 1;
-		result = prime * result + ((id == null) ? 0 : id.hashCode());
-		return result;
-	}
+		@Override
+		public int hashCode() {
+			final int prime = 31;
+			int result = 1;
+			result = prime * result + ((id == null) ? 0 : id.hashCode());
+			return result;
+		}
 
-	@Override
-	public boolean equals(Object obj) {
-		if (this == obj)
-			return true;
-		if (obj == null)
-			return false;
-		if (getClass() != obj.getClass())
-			return false;
-		Participant other = (Participant) obj;
-		if (id == null) {
-			if (other.id != null)
+		@Override
+		public boolean equals(Object obj) {
+			if (this == obj)
+				return true;
+			if (obj == null)
 				return false;
-		} else if (!id.equals(other.id))
-			return false;
-		return true;
-	}
+			if (getClass() != obj.getClass())
+				return false;
+			Participant other = (Participant) obj;
+			if (id == null) {
+				if (other.id != null)
+					return false;
+			} else if (!id.equals(other.id))
+				return false;
+			return true;
+		}
 
-	@Override
-	public String toString() {
-		return "Participant [nombre=" + nombre + ", apellidos=" + apellidos + ", fechaNacimiento=" + fechaNacimiento + ", email=" + email + ", DNI=" + DNI + ", direccion="
-				+ direccion + ", nacionalidad=" + nacionalidad + ", isAdmin=" + isAdmin + ", isPolitician="
-				+ isPolitician + "]";
-	}
+		@Override
+		public String toString() {
+			return "Participant [nombre=" + nombre + ", localizacion=" + localizacion + ", email=" + email
+					+ ", identificador=" + identificador + ", tipo=" + tipo + ", tipoCode=" + tipoCode + ", password="
+					+ password + "]";
+		}
 
 }
