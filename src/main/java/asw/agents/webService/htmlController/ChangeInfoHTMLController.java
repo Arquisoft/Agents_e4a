167a6f29776a47f4076c1fa13249b436fae5250a
UO237674 <UO237674@EII-L14-13.aulasuo.uniovi.es>
2018-04-20 10:11:57 +0200

    redirect si falla

--- src/main/java/asw/agents/webService/htmlController/ChangeInfoHTMLController.java
+++ src/main/java/asw/agents/webService/htmlController/ChangeInfoHTMLController.java
@@ -6,6 +6,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
@@ -23,7 +24,10 @@ public class ChangeInfoHTMLController {
 	private UpdateInfo updateInfo;
 
 	@RequestMapping(value = "/changeInfo", method = RequestMethod.POST)
-	public String changeInfo() {
+	public String changeInfo(BindingResult result) {
+		if (result.hasErrors()) {
+			return "login";
+		}
 		return "changeInfo";
 	}
 
