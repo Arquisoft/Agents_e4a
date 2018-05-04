d8efd61979b5474082677249658ea288cb5b923b
Pelayo <pelayo.garcia97@gmail.com>
2018-05-04 21:29:35 +0200

    Añadida la interfaz GetAgent
    Tuve que añadirla yo a mano otra vez, porque no me dejaba pullear el commit de Gonzalo

--- src/main/java/asw/dbManagement/impl/GetAgentImpl.java
+++ src/main/java/asw/dbManagement/impl/GetAgentImpl.java
@@ -6,12 +6,13 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import asw.dbManagement.GetAgent;
 import asw.dbManagement.model.Agent;
 import asw.dbManagement.repository.AgentRepository;
 
 
 @Service
-public class GetAgentImpl{
+public class GetAgentImpl implements GetAgent{
 	
 	@Autowired
 	private AgentRepository repository;
@@ -21,11 +22,13 @@ public class GetAgentImpl{
 	 * Método que devuelve el Agente buscado por identificador
 	 * Hace uso del método findByIdentificador (mapeador)
 	 */
+	@Override
 	public Agent getAgent(String identificador) {
 		
 		return repository.findById(identificador);
 	}
 	
+	@Override
 	public List<Agent> getAgents(){
 		List<Agent> agents = new ArrayList<Agent>();
 		repository.findAll().forEach(agents::add);
